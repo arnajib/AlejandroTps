458cdc434f0a51e79e7d6cccc8d0419ffb2de309
mahdi <mahdi.zolnouri@polymtl.ca>
2016-02-06 23:30:01 -0500

    J'arrive maintenant a remplacer une case de gridview par un autre case. L'idée de tracer une ligne entre deux boules est de remplacer l'image de chaque case séléctionné par une image convenable

--- FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/Game.java
+++ FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/Game.java
@@ -10,8 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+import com.tp1.inf8405.flowfree.ImageAdapter;
 
 public class Game extends Activity {
+    int i=0;
+    int firstClick,secondClick;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,34 @@ public class Game extends Activity {
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
-
+        final ImageAdapter im = new ImageAdapter(this);
+        im.notifyDataSetChanged();
+        gridview.setAdapter(im);
+        gridview.invalidateViews();
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
                 Toast.makeText(Game.this, "" + position,
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+                // TODO Auto-generated method stub
+                i++;
+                if( i %2!=0){
+                    firstClick=arg2;
+                }else{
+                    secondClick=arg2;
+                    Integer help=new Integer((Integer) im.getItem(firstClick));
+                    im.setItem(firstClick, secondClick);
+                    im.setItemInteger(secondClick, help);
+                    im.notifyDataSetChanged();
+                    System.out.println("Second Click "+i);
+                }
+
             }
         });
     }
--- FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/ImageAdapter.java
+++ FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/ImageAdapter.java
@@ -3,15 +3,20 @@ package com.tp1.inf8405.flowfree;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 /**
  * Created by najib on 04/02/16.
  */
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private int lastClicked;
+    ImageView iView;
 
     public ImageAdapter(Context c) {
         mContext = c;
@@ -22,10 +27,17 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     public Object getItem(int position) {
-        return null;
+
+        System.out.println("Item Is :-"+mThumbIds[position].toString());
+        return mThumbIds[position];
     }
 
     public long getItemId(int position) {
+        System.out.println("Geting Id of Item "+mThumbIds[position]);
+        if(iView != null){
+            iView.setImageResource(mThumbIds[0]);
+            Toast.makeText(mContext, "Call", Toast.LENGTH_SHORT).show();
+        }
         return 0;
     }
 
@@ -46,6 +58,20 @@ public class ImageAdapter extends BaseAdapter {
         return imageView;
     }
 
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View v, int position,
+//                            long id) {
+//        if (lastClicked != -1) {
+//            // we previously selected a position
+//            mThumbIds[lastClicked] = R.drawable.default_image;
+//        }
+//        if (lastClicked != position) {
+//            mThumbIds[position] = R.drawable.image_required_when_grid_selected;
+//        }
+//        lastClicked = position;
+//        mAdapter.notifyDataSetChanged(); // mAdapter is a reference to the GridView's adapter
+//    }
+
     // references to our images
     private Integer[] mThumbIds = {
             R.drawable.white, R.drawable.white, R.drawable.pink, R.drawable.white, R.drawable.white, R.drawable.green, R.drawable.blue,
@@ -56,4 +82,12 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.white, R.drawable.white, R.drawable.white, R.drawable.white, R.drawable.white,R.drawable.white, R.drawable.white,
             R.drawable.white, R.drawable.white, R.drawable.blue, R.drawable.white, R.drawable.green, R.drawable.white, R.drawable.white
     };
+
+    public void setItem(int firstClick, int secondClick) {
+        mThumbIds[firstClick] = mThumbIds[secondClick];
+    }
+
+    public void setItemInteger(int secondClick, Integer help) {
+        mThumbIds[secondClick]=help;
+    }
 }
