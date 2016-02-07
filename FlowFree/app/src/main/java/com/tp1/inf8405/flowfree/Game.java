d339836973ac9aed7363a2e6d8d7dd55855143fa
mahdi <mahdi.zolnouri@polymtl.ca>
2016-02-07 15:11:37 -0500

    Regler le problème de touching lors de toucher un cell. Maintenant on peut cliquer puis app enregistre le touchDown position puis lorsque tu bouge ton doigt et tu enleve ton doigt dans un autre case, il trouve position de touchUp et il change l'image de cette case par une image aleatoire

--- FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/Game.java
+++ FlowFree/app/src/main/java/com/tp1/inf8405/flowfree/Game.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 import com.tp1.inf8405.flowfree.ImageAdapter;
 
@@ -18,95 +19,100 @@ public class Game extends Activity {
     int firstClick,secondClick;
     int firstTouch,secondTouch;
     GridView gridview;
-
+    final ImageAdapter im = new ImageAdapter(this);
+    int posTouchDown = 0;
+    int posTouchUp = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
         gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
-        final ImageAdapter im = new ImageAdapter(this);
-        im.notifyDataSetChanged();
         gridview.setAdapter(im);
         gridview.invalidateViews();
+        addListenerToGrid();
+        im.notifyDataSetChanged();
 
-        gridview.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Toast.makeText(parent.getContext(), "Spinner item 1!", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 1:
-                        Toast.makeText(parent.getContext(), "Spinner item 2!", Toast.LENGTH_SHORT).show();
-                        break;
-                    case 2:
-                        Toast.makeText(parent.getContext(), "Spinner item 3!", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(Game.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-//        gridview.setOnTouchListener(new AdapterView.OnTouchListener() {
+//        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
 //            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                j++;
-//                if ()
-//                    return false;
+//            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//                i++;
+//                if (i % 2 != 0) {
+//                    firstClick = arg2;
+//                    Toast.makeText(Game.this, "" + arg2,
+//                            Toast.LENGTH_SHORT).show();
+//                } else {
+//                    secondClick = arg2;
+//                    Integer help = new Integer((int) im.getItemIdTabPrincipal(firstClick));
+//                    im.setItemInTabPrincipal(firstClick, secondClick);
+//                    im.setItemInteger(secondClick, help);
+//                    im.notifyDataSetChanged();
+//
+//                }
+//
 //            }
 //        });
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                i++;
-                if (i % 2 != 0) {
-                    firstClick = arg2;
-                } else {
-                    secondClick = arg2;
-                    Integer help = new Integer((int) im.getItemIdTabPrincipal(firstClick));
-                    im.setItemInTabPrincipal(firstClick, secondClick);
-                    im.setItemInteger(secondClick, help);
-                    im.notifyDataSetChanged();
-                    System.out.println("Second Click " + i);
-                }
-
-            }
-        });
     }
     public void addListenerToGrid() {
-        gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-        final ImageAdapter im2 = new ImageAdapter(this);
-        im2.notifyDataSetChanged();
         gridview.setOnTouchListener(new View.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent me) {
 
                 int action = me.getActionMasked();
-                float currentXPosition = me.getX();
-                float currentYPosition = me.getY();
-                int position = gridview.pointToPosition((int) currentXPosition, (int) currentYPosition);
-                if (action == MotionEvent.ACTION_UP) {
-                    // Key was pressed here
-                    im2.setItemInTabPrincipal(position, position) ;
+                float currentXPosition;
+                float currentYPosition;
+
+
+//                if (me.getAction() == MotionEvent.ACTION_DOWN) {
+//                    //Toast.makeText(Game.this, " Touch Down: " + position,Toast.LENGTH_SHORT).show();
+//                }else if (me.getAction() == MotionEvent.ACTION_MOVE){
+//                    //Toast.makeText(Game.this, " Moving: " + position,Toast.LENGTH_SHORT).show();
+//                }else if (me.getAction() == MotionEvent.ACTION_UP){
+//                    //Toast.makeText(Game.this, " Up: " + position,Toast.LENGTH_SHORT).show();
+//                }
+                switch (me.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        currentXPosition = me.getX();
+                        currentYPosition = me.getY();
+                        posTouchDown = gridview.pointToPosition((int) currentXPosition, (int) currentYPosition);
+                        Toast.makeText(Game.this, " Touch Down: " + posTouchDown,Toast.LENGTH_SHORT).show();
+                        break;
+//                    case MotionEvent.ACTION_UP:
+//                        //Toast.makeText(Game.this, " Moving: ",Toast.LENGTH_SHORT).show();
+//                        break;
+                    case MotionEvent.ACTION_UP:
+                        currentXPosition = me.getX();
+                        currentYPosition = me.getY();
+                        posTouchUp = gridview.pointToPosition((int) currentXPosition, (int) currentYPosition);
+                        Integer help = new Integer((int) im.getItemIdTabPrincipal(posTouchDown));
+                        im.setItemInTabPrincipal(posTouchUp, posTouchDown);
+                        //im.setItemInteger(posTouchUp, help);
+                        im.notifyDataSetChanged();
+                        Toast.makeText(Game.this, " Up: " + posTouchUp,Toast.LENGTH_SHORT).show();
+                        break;
                 }
 
                 return true;
             }
         });
     }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        int x = (int) event.getX();         // NOTE: event.getHistorical... might be needed.
+//        int y = (int) event.getY();
+//        int position = gridview.pointToPosition((int) x, (int) y);
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//            Toast.makeText(Game.this, " Touch Down: " + position,Toast.LENGTH_SHORT).show();
+//        }else if (event.getAction() == MotionEvent.ACTION_MOVE){
+//            Toast.makeText(Game.this, " Moving: " + position,Toast.LENGTH_SHORT).show();
+//        }
+//
+//        return true;
+//    }
 }
 
